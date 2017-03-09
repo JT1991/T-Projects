d34f5375972db704deb433f438aa0e715ad441b9
josh thorn <joshthorn91@gmail.com>
2017-03-09 17:27:32 +0000

    javascript basics

---
+++ Javascript Basics - Variable challenge/story.js
@@ -0,0 +1,7 @@
+var adjv = prompt("Please type an adjective.")
+var verb = prompt("Please type a verb.")
+var noun = prompt("Please type a noun.")
+var sentence = "<h2> There once was a " + adjv;
+sentence += " programmer who wanted to use his coding skills to " + verb;
+sentence += " the " + noun;
+document.write(sentence)
