db13563269936480d64380fcd02e3a31bdc0dec8
josh thorn <joshthorn91@gmail.com>
2018-02-27 20:27:28 +0000

    treetunes upload functionality
    UNSECURE.

--- Javascript OOP - Playlist/playlist/playlistSongs.js
+++ Javascript OOP - Playlist/playlist/playlistSongs.js
@@ -1,8 +1,1 @@
-var SchizoidMan = new Song("21st Century Schizoid Man Including Mirrors", "King Crimson", "7:00", "audio/1.mp3");
-var TalkWind = new Song("I Talk To The Wind", "King Crimson", "3:43", "audio/2.mp3");
-var Warmest = new Movie("Blue Is the Warmest Colour", 2011, "2:13:00", "audio/3.mkv");
-
-playlist.add(SchizoidMan);
-playlist.add(TalkWind);
-playlist.add(Warmest);
 
